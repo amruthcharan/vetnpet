0f285b854179db5e0d108e95f8f556791ce36351
Amruth Charan <amruthcharan1@gmail.com>
2021-12-10 19:07:23 +0530

    Fixed date selection

--- resources/views/appointments/api.blade.php
+++ resources/views/appointments/api.blade.php
@@ -243,7 +243,7 @@
     }
 	$(document).ready(function () {		
 		var today = new Date();
-		var dd = today.getDate()+1;
+		var dd = today.getDate();
 		var mm = today.getMonth()+1;
 		var yyyy = today.getFullYear();
 		
